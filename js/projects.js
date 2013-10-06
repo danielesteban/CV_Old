074adab519c4c49cd07160b553a48a57f597626d
Daniel Esteban Nombela <dani@hellacoders.com>
2013-10-05 22:18:27 -0600

    more projects.

--- js/projects.js
+++ js/projects.js
@@ -1,129 +1,6 @@
 PROJECTS = [
 	{
 		media : [
-			{ image : 'cayon.jpg' }
-		],
-		thumb : 'cayon',
-		titleEN : 'Cayón Numismática',
-		links : [
-			{
-				url : 'http://cayon.com',
-				titleEN : 'Cayon.com'
-			}
-		],
-		tags : [
-			'PHP',
-			'MySQL',
-			'JS',
-			'HTML5',
-			'Bootstrap'
-		]
-	},
-	{
-		media : [
-			{ image : 'suitz.jpg' },
-			{ image : 'suitz2.jpg' },
-			{ image : 'suitz3.jpg' }
-		],
-		thumb : 'suitz',
-		titleEN : 'Suitz online shop',
-		titleES : 'Tienda online Suitz',
-		descEN : 'Suitz its a firm that decided to move their business to the web in order to attract new demographies. Their new online service features a complete store management system.',
-		descES : 'Suitz es una firma que ha decidido mover su negocio a la web para atraer a nuevas demografías. Su nueva servicio online cuenta con un gestor completo de toda la tienda.',
-		roleEN : 'Project Analysis. Full site developement based on client\'s style guide. Content, promotions & orders management system. Payment Gateway. Security consultancy.',
-		roleES : 'Análisis del proyecto. Desarrollo completo del site siguiendo la guía de estilos del cliente. Gestor de contenidos, promociones y pedidos. Pasarela de pago. Consultoría en seguridad.',
-		links : [
-			{
-				url : 'http://suitz.com',
-				titleEN : 'Suitz.com'
-			}
-		],
-		tags : [
-			'PHP',
-			'MySQL',
-			'JS',
-			'HTML5',
-			'Paypal'
-		]
-	},
-	{
-		media : [
-			{ image : 'amiformacion.jpg' },
-			{ image : 'amiformacion1.jpg' }
-		],
-		thumb : 'amiformacion',
-		titleEN : 'AMI Formación',
-		descEN : 'E-learning platform for AMI consultores S.L., a company dedicated to certify and teach about quality and the environment.',
-		descES : 'Plataforma de e-learning para AMI consultores S.L., empresa dedicada a la certificación y formación en calidad y medio ambiente.',
-		roleEN : 'Full site design and developement. Multianswer test system. Courses & students management system. Mobile & tablet design responsiveness.',
-		roleES : 'Diseño y desarrollo completo del site. Sistema de tests multirespuesta. Gestor de cursos y alumnos. Adaptabilidad del diseño para móvil y tablet.',
-		links : [
-			{
-				url : 'http://formacion.amiconsultores.com',
-				titleEN : 'formacion.amiconsultores.com'
-			}
-		],
-		tags : [
-			'PHP',
-			'MySQL',
-			'JS',
-			'HTML5',
-			'Bootstrap'
-		]
-	},
-	{
-		media : [
-			{ video : 'www.youtube.com/embed/JFCWeERFWIw' },
-			{ video : 'www.youtube.com/embed/C_XJvi66Fws' }
-		],
-		thumb : 'ledtunes',
-		titleEN : 'LedTunes',
-		descEN : 'Convert music into light!.. LedTunes is a Software&Hardware Prototype that plays audio from your hard drive and sends its FFT to an Arudino Mega that generates realtime animations multiplexing a 32x32 led panel.',
-		descES : '¡Convierte tu música en luz!.. LedTunes es un prototipo de Software&Hardware que reproduce audio desde tu disco duro y manda su FFT a una Arudino Mega que genera animaciones en tiempo real multiplexando un panel led de 32x32.',
-		roleEN : 'First I designed a shield to connect the led panel. Then, an Arduino sketch that receives signals trough serial (Usb/Bluetooth). Finally, a desktop application for OSX, Linux & Windows.',
-		roleES : 'Primero diseñe un shield para conectar el panel led. Luego, un sketch de Arduino que recibe señales por serie (Usb/Bluetooth). Finalmente, una apliación desktop para OSX, Linux y Windows.',
-		links : [
-			{
-				url : 'https://github.com/danielesteban/LedTunes',
-				titleEN : 'Source code on Github',
-				titleES : 'Código fuente en Github'
-			}
-		],
-		tags : [
-			'C++',
-			'Arduino',
-			'Hardware',
-			'Node-Webkit'
-		]
-	},
-	{
-		media : [
-			{ image : 'derechoalcuento.jpg' },
-			{ image : 'derechoalcuento1.jpg' },
-			{ image : 'derechoalcuento2.jpg' }
-		],
-		thumb : 'derechoalcuento',
-		titleEN : 'Derecho Al Cuento',
-		descEN : 'A platform that seeks that all children regardless of their personal situation can enjoy a narrated story.',
-		descES : 'Una plataforma que busca que todos los niños sin importar su situación personal puedan disfrutar de un cuento narrado.',
-		roleEN : 'HTML5 audio player integration with SoundManager2. AS3 audio recorder. UI Logic & animations. RPC server development in PHP.',
-		roleES : 'Integración del reproductor de audio HTML5 con SoundManager2. Grabador de audio AS3. Lógica y animaciones del UI. Desarrollo del servidor RPC en PHP.',
-		links : [
-			{
-				url : 'http://derechoalcuento.com',
-				titleEN : 'Derechoalcuento.com'
-			}
-		],
-		tags : [
-			'PHP',
-			'MySQL',
-			'JS',
-			'CSS3',
-			'AS3'
-		]
-	},
-	{
-		media : [
 			{ image : 'gatunes.jpg' },
 			{ image : 'gatunes1.jpg' }
 		],
@@ -179,7 +56,8 @@ PROJECTS = [
 			'CSS3',
 			'FB'
 		]
-	},{
+	},
+	{
 		media : [
 			{ image : 'remoteChalkboard.jpg' },
 			{ image : 'remoteChalkboard1.jpg' },
@@ -187,6 +65,10 @@ PROJECTS = [
 		],
 		thumb : 'remoteChalkboard',
 		titleEN : 'RemoteChalkboard',
+		descEN : 'A multi-user doodling platform. Enables kids to do collective art pieces. Each with their individual layer. It runs on any desktop/mobile browser that supports canvas. Hosted on openshift.',
+		descES : 'Una plataforma de dibujo multi-usuario. Permite a los niños crear piezas de arte colectivo. Casa uno con su capa individual. Funciona en cualquier navegador desktop/mobile que soporte canvas. Alojado en openshift.',
+		roleEN : 'I coded this opensource experiment to learn more about canvas, brushes and touchevents. Also took the opportunity to test deploying it on redhat\'s cloud.',
+		roleES : 'Programé este experimento opensource para aprender más sobre canvas, pinceles y touchevents. También aproveche la oportunidad para probar el deploy en la nube de redhat.',
 		links : [
 			{
 				url : 'http://remotechalkboard-hellaserver.rhcloud.com/',
@@ -202,15 +84,102 @@ PROJECTS = [
 	},
 	{
 		media : [
-			{ image : 'gameengine.jpg' }
+			{ video : 'www.youtube.com/embed/JFCWeERFWIw' },
+			{ video : 'www.youtube.com/embed/C_XJvi66Fws' }
 		],
-		thumb : 'gameengine',
-		titleEN : 'JS game engine',
+		thumb : 'ledtunes',
+		titleEN : 'LedTunes',
+		descEN : 'Convert music into light!.. LedTunes is a Software&Hardware Prototype that plays audio from your hard drive and sends its FFT to an Arudino Mega that generates realtime animations multiplexing a 32x32 led panel.',
+		descES : '¡Convierte tu música en luz!.. LedTunes es un prototipo de Software&Hardware que reproduce audio desde tu disco duro y manda su FFT a una Arudino Mega que genera animaciones en tiempo real multiplexando un panel led de 32x32.',
+		roleEN : 'First I designed a shield to connect the led panel. Then, an Arduino sketch that receives signals trough serial (Usb/Bluetooth). Finally, a desktop application for OSX, Linux & Windows.',
+		roleES : 'Primero diseñe un shield para conectar el panel led. Luego, un sketch de Arduino que recibe señales por serie (Usb/Bluetooth). Finalmente, una apliación desktop para OSX, Linux y Windows.',
+		links : [
+			{
+				url : 'https://github.com/danielesteban/LedTunes',
+				titleEN : 'Source code on Github',
+				titleES : 'Código fuente en Github'
+			}
+		],
 		tags : [
+			'C++',
+			'Arduino',
+			'Hardware',
+			'Node-Webkit'
+		]
+	},
+	{
+		media : [
+			{ image : 'suitz.jpg' },
+			{ image : 'suitz2.jpg' },
+			{ image : 'suitz3.jpg' }
+		],
+		thumb : 'suitz',
+		titleEN : 'Suitz online shop',
+		titleES : 'Tienda online Suitz',
+		descEN : 'Suitz its a firm that decided to move their business to the web in order to attract new demographies. Their new online service features a complete store management system.',
+		descES : 'Suitz es una firma que ha decidido mover su negocio a la web para atraer a nuevas demografías. Su nueva servicio online cuenta con un gestor completo de toda la tienda.',
+		roleEN : 'Project Analysis. Full site developement based on client\'s style guide. Content, promotions & orders management system. Payment Gateway. Security consultancy.',
+		roleES : 'Análisis del proyecto. Desarrollo completo del site siguiendo la guía de estilos del cliente. Gestor de contenidos, promociones y pedidos. Pasarela de pago. Consultoría en seguridad.',
+		links : [
+			{
+				url : 'http://suitz.com',
+				titleEN : 'Suitz.com'
+			}
+		],
+		tags : [
+			'PHP',
+			'MySQL',
 			'JS',
 			'HTML5',
-			'MelonJS',
-			'Node-Webkit'
+			'Paypal'
+		]
+	},
+	{
+		media : [
+			{ image : 'usynth.jpg' }
+		],
+		thumb : 'usynth',
+		titleEN : 'µSynth',
+		descEN : 'The world\'s smallest/cheapest synthesizer. Features 2 independent synths with 8Bit square waves and 8Khz samplerate. All this for just ~10$ of electronics (microcontroller included).',
+		descES : 'El sintetizador más pequeño/barato del mundo. Cuenta con 2 sintes independientes con 8Bit square waves y 8Khz samplerate. Todo esto por sólo ~10$ (microcontrolador incluído).',
+		roleEN : 'Complete circuit design with an R-2R DAC as output in less than 4x3cm. Function generation library. Synth software with several musical scales and multiple input methods, including a theremin-style control.',
+		roleES : 'Diseño completo del circuto con una DAC R-2R como output en menos de 4x3cm. Librería de generación de funciones. Synth software con multiples escalas musicales y diversos métodos de entrada, incluyendo un control estilo theremin.',
+		links : [
+			{
+				url : 'https://github.com/danielesteban/uSynth',
+				titleEN : 'Source code on Github',
+				titleES : 'Código fuente en Github'
+			}
+		],
+		tags : [
+			'C++',
+			'Arduino',
+			'Hardware'
+		]
+	},
+	{
+		media : [
+			{ image : 'amiformacion.jpg' },
+			{ image : 'amiformacion1.jpg' }
+		],
+		thumb : 'amiformacion',
+		titleEN : 'AMI Formación',
+		descEN : 'E-learning platform for AMI consultores S.L., a company dedicated to certify and teach about quality and the environment.',
+		descES : 'Plataforma de e-learning para AMI consultores S.L., empresa dedicada a la certificación y formación en calidad y medio ambiente.',
+		roleEN : 'Full site design and developement. Multianswer test system. Courses & students management system. Mobile & tablet design responsiveness.',
+		roleES : 'Diseño y desarrollo completo del site. Sistema de tests multirespuesta. Gestor de cursos y alumnos. Adaptabilidad del diseño para móvil y tablet.',
+		links : [
+			{
+				url : 'http://formacion.amiconsultores.com',
+				titleEN : 'formacion.amiconsultores.com'
+			}
+		],
+		tags : [
+			'PHP',
+			'MySQL',
+			'JS',
+			'HTML5',
+			'Bootstrap'
 		]
 	},
 	{
@@ -219,10 +188,19 @@ PROJECTS = [
 		],
 		thumb : 'webchat',
 		titleEN : 'WebChat',
+		descEN : 'An anonymous video-chat service. Fully HTML5 (without flash). It also features many video filters, including a chroma-key background replace tool, like the one on Apple\'s photobooth.',
+		descES : 'Un servicio anónimo de video-chat. Totalmente HTML5 (sin flash). También cuenta con varios filtros de vídeo, incluyendo una utilidad chroma-key para reemplazar el fondo, como la de el photobooth de Apple.',
+		roleEN : 'I developed this project to test the capabilities of HTML5 and canvas, learn more about image processing & filters and to chat with my buddies without being spied by the NSA ;P',
+		roleES : 'Desarrollé este proyecto para probar las capacidades de HTML5 y canvas, aprender mas sobre procesamiento y filtros de imágenes y para chatear con mis amigos sin ser espiado por la NSA ;P',
 		links : [
 			{
 				url : 'http://chat.hellacoders.com/',
 				titleEN : 'chat.hellacoders.com'
+			},
+			{
+				url : 'https://github.com/danielesteban/WebChat',
+				titleEN : 'Source code on Github',
+				titleES : 'Código fuente en Github'
 			}
 		],
 		tags : [
@@ -234,21 +212,63 @@ PROJECTS = [
 	},
 	{
 		media : [
-			{ image : 'inncive.jpg' }
+			{ image : 'cayon.jpg' }
 		],
-		thumb : 'inncive',
-		titleEN : 'INNCIVE',
+		thumb : 'cayon',
+		titleEN : 'Cayón Numismática',
+		descEN : 'An online auctions platform built from the ground up. Features the basics of any auction site, plus a private registration/validation system, several statistics and auto-promotion of trending articles.',
+		descES : 'Una plataforma online de subastas construida desde cero. Cuenta con los elementos básicos de cualquier sitio de subastas, más un sistema de registro/validación privado, multiples estadísticas y auto-promoción basada en tendencias.',
+		roleEN : 'Project Analysis. Full site developement based on client\'s style guide. Catalogs, users & auctions management system. Bookmarking (follow articles) system. Security consultancy.',
+		roleES : 'Análisis del proyecto. Desarrollo completo del site siguiendo la guía de estilos del cliente. Gestor de catálogos, usuarios y subastas. Sistema de favoritos (seguir artículos). Consultoría en seguridad.',
+		tags : [
+			'PHP',
+			'MySQL',
+			'JS',
+			'HTML5',
+			'Bootstrap'
+		]
+	},
+	{
+		media : [
+			{ image : 'derechoalcuento.jpg' },
+			{ image : 'derechoalcuento1.jpg' },
+			{ image : 'derechoalcuento2.jpg' }
+		],
+		thumb : 'derechoalcuento',
+		titleEN : 'Derecho Al Cuento',
+		descEN : 'A platform that seeks that all children regardless of their personal situation can enjoy a narrated story.',
+		descES : 'Una plataforma que busca que todos los niños sin importar su situación personal puedan disfrutar de un cuento narrado.',
+		roleEN : 'HTML5 audio player integration with SoundManager2. AS3 audio recorder. UI Logic & animations. RPC server development in PHP.',
+		roleES : 'Integración del reproductor de audio HTML5 con SoundManager2. Grabador de audio AS3. Lógica y animaciones del UI. Desarrollo del servidor RPC en PHP.',
 		links : [
 			{
-				url : 'http://inncive.com',
-				titleEN : 'Inncive.com'
+				url : 'http://derechoalcuento.com',
+				titleEN : 'Derechoalcuento.com'
 			}
 		],
 		tags : [
 			'PHP',
 			'MySQL',
 			'JS',
-			'CSS3'
+			'CSS3',
+			'AS3'
+		]
+	},
+	{
+		media : [
+			{ image : 'gameengine.jpg' }
+		],
+		thumb : 'gameengine',
+		titleEN : 'JS game engine',
+		descEN : 'An opensource engine based on MelonJS. It personalizes the player look taking a photo from his webcam. Full platform side-scroller physics. Supports Tiled editor maps.',
+		descES : 'Un motor opensource basado en MelonJS. Personaliza el aspecto del jugador tomando una fotografía de su webcam. Físicas plataformas side-scroller. Soporta mapas de Tiled editor.',
+		roleEN : 'Most of the sprites, graphics and sounds. Player & enemies behavior. Parallax background. 3 test maps. Node-Webkit encapsulation.',
+		roleES : 'Mayoría de los sprites, gráficos y sonidos. Comportamiento del jugador y los enemigos. Fondo parallax. 3 mapas de test. Encapsulación en Node-Webkit',
+		tags : [
+			'JS',
+			'HTML5',
+			'MelonJS',
+			'Node-Webkit'
 		]
 	},
 	{
@@ -257,6 +277,10 @@ PROJECTS = [
 		],
 		thumb : 'tuenti',
 		titleEN : 'Tuenti',
+		descEN : 'Tuenti helps you share experiences with the people that really matter to you - your real friends - in the simplest way possible. Their mobile and web multi-platform apps also let you chat for free and in real time with whoever you want.',
+		descES : 'Tuenti es la forma más sencilla de compartir tus experiencias con las personas que realmente te importan: tus amigos de verdad. Además, a través de su multiplataforma de aplicaciones móviles y web puedes chatear gratis y en tiempo real con quien quieras.',
+		roleEN : 'I was the first hired (non-founder) programmer. The 2008 version of the site was entirely built by myself and another programmer. Also did lots of UI and usability improvements to the site.',
+		roleES : 'Fuí el primer programador non-founder. La versión de 2008 del site fué construida enteramente por mí y otro programador. También hice bastantes mejoras en el UI y la usabilidad del site.',
 		links : [
 			{
 				url : 'http://tuenti.com',
@@ -269,14 +293,28 @@ PROJECTS = [
 			'JS',
 			'CSS3'
 		]
+	},
+	{
+		media : [
+			{ image : 'inncive.jpg' }
+		],
+		thumb : 'inncive',
+		titleEN : 'INNCIVE',
+		descEN : 'Corporate site & intranet for an engineering and consulting firm.',
+		descES : 'Sitio corporativo e intranet para una firma de ingeniería y consultoría.',
+		roleEN : 'Project Analysis. Full site developement based on client\'s style guide. Content management system. Internationalization system.',
+		roleES : 'Análisis del proyecto. Desarrollo completo del site siguiendo la guía de estilos del cliente. Gestor de contenidos. Sistema de internacionalización.',
+		links : [
+			{
+				url : 'http://inncive.com',
+				titleEN : 'Inncive.com'
+			}
+		],
+		tags : [
+			'PHP',
+			'MySQL',
+			'JS',
+			'CSS3'
+		]
 	}
 ];
-
-var debugDesc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae laoreet dolor, ac egestas nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
-
-PROJECTS.forEach(function(p) {
-	!p.descEN && (p.descEN = debugDesc);
-	!p.descES && (p.descES = debugDesc);
-	!p.roleEN && (p.roleEN = debugDesc);
-	!p.roleES && (p.roleES = debugDesc);
-});
